4a9bf8c47f26543ff3025d0a861a4ad6b65e0567
Glory ann Conwi <gaconwi@googlemail.com>
2023-12-21 12:04:41 +0100

    Challenge: Backend create products

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -3,6 +3,16 @@ import Product from "../../../db/models/Product";
 
 export default async function handler(request, response) {
   await dbConnect();
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+      response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
 
   if (request.method === "GET") {
     const products = await Product.find();
